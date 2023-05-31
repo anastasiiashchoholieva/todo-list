03ecfec8f94bd1de3d5be23d5612955acdee6c93
Anastasiia <nastyashch1601@gmail.com>
2023-05-31 12:49:12 +0100

    Add static files directory

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "todolist",
 ]
 
@@ -71,6 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "todo_list.wsgi.application"
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
